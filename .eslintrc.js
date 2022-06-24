3ec1beffc80b0d30864a8ed4a5e3e20e8b1a8aa7
vishal-kaundal <vishal2661999@gmail.com>
2022-06-24 14:19:27 +0530

    add tailwind

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  plugins: ['prettier', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'airbnb-base',
+    'expo/native',
+    'expo',
+    'plugin:react/jsx-runtime',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+  },
+  rules: {
+    indent: 0,
+    'no-tabs': 0,
+    'no-underscore-dangle': 0,
+    'space-before-function-paren': 0,
+    'quote-props': 0,
+    'comma-dangle': 0,
+    'operator-linebreak': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/forbid-prop-types': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/label-has-for': 0,
+    'class-methods-use-this': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+    'arrow-parens': 0,
+    'object-curly-newline': 0,
+    'react/no-children-prop': 0,
+    'function-paren-newline': 0,
+    'implicit-arrow-linebreak': 0,
+    'import/prefer-default-export': 0,
+    'no-confusing-arrow': 0,
+    'react/prop-types': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        parser: 'flow',
+      },
+    ],
+  },
+};
--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+// import { styled } from 'tailwindcss-react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
+
+// const StyledText = styled(Text)`
+//   color: '#848487';
+// `;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-bold">
+          Open up App.js to start working on your App!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </TailwindProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
